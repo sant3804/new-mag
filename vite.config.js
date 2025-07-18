57bfc67f362a11ccc31479d35c36f40d3797a8f5
Santosh Kumar <144969293+sant3804@users.noreply.github.com>
2025-07-18 15:47:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Newsweb/',  // ✅ Must match your repo name exactly
   plugins: [react()],
+  base: '/', // or remove 'base' line entirely
 })
